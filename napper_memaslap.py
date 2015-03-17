4f4636a04b01ee0ebe45c55c98328540be13e990
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2015-03-17 01:53:50 +0000

    Add wrapper for memaslap load tester.

---
+++ napper_memaslap.py
@@ -0,0 +1,55 @@
+import sys, socket, time, logging
+import shlex, subprocess
+from kazoo.client import KazooClient
+from kazoo.exceptions import NodeExistsError
+
+def zkConnect(conn_str):
+  zk = KazooClient(hosts=conn_str)
+  zk.start()
+  return zk
+
+def zkCreateJobDir(zk, job_name):
+  zk.ensure_path("/napper/memaslap/%s" % (job_name))
+
+def zkRemoveJobDir(zk, job_name):
+  zk.delete("/napper/memaslap/%s" % (job_name), recursive=True)
+
+def zkRegisterWorker(zk, job_name, hostname, port):
+  print "Registering myself as %s:%d" % (hostname, port)
+  zk.create("/napper/memaslap/%s/%s:%d" % (job_name, hostname, port), "%d" % (port), ephemeral=True)
+  return port
+
+logging.basicConfig()
+
+if len(sys.argv) < 6:
+  print "usage: napper_memaslap <Zookeeper hostname:port> <job name> <worker ID> <num workers> <executable>"
+  sys.exit(1)
+
+hostport = sys.argv[1]
+job_name = sys.argv[2]
+worker_id = int(sys.argv[3])
+num_workers = int(sys.argv[4])
+memaslap_path = " ".join(sys.argv[5:])
+
+client = zkConnect(hostport)
+zkCreateJobDir(client, job_name)
+
+hosts = []
+done = False
+children = client.get_children("/napper/memcached/")
+for c in children:
+  if not ":" in c:
+    continue
+  data, stat = client.get("/napper/memcached/%s" % (c))
+  print "%s:%s" % (c, data)
+  hosts.append("%s" % (c))
+
+# execute program
+command = "%s -s %s -T 1 -t 10s" % (memaslap_path, ",".join(hosts))
+print "RUNNING: %s" % (command)
+subprocess.call(shlex.split(command))
+
+# this will implicitly clean up afterwards
+client.stop()
+
+sys.exit(0)
