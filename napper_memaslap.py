69c262ffcebc7f9c88abe8f1cb84797cdc77d386
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2015-03-18 00:23:41 +0000

    Don't mandate single-threaded operation of memaslap.

--- napper_memaslap.py
+++ napper_memaslap.py
@@ -45,7 +45,7 @@ for c in children:
   hosts.append("%s" % (c))
 
 # execute program
-command = "%s -s %s -T 1" % (memaslap_path, ",".join(hosts))
+command = "%s -s %s" % (memaslap_path, ",".join(hosts))
 print "RUNNING: %s" % (command)
 subprocess.call(shlex.split(command))
 
