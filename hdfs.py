94c149f950a24a5034082c9b177037307f9ed809
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2015-03-19 23:08:19 +0000

    Add some timing and fix debug output from HDFS client.

--- hdfs.py
+++ hdfs.py
@@ -1,14 +1,19 @@
 import sys, os
 import shlex, subprocess
+import time
 
 def hdfs_fetch_file(hdfs_path, local_path):
-  print "Getting %s..."
+  print "Getting %s..." % (hdfs_path)
+  start = time.time()
   command = "hadoop fs -get %s %s" % (hdfs_path, local_path)
   subprocess.call(shlex.split(command))
-  print "Done getting %s..."
+  end = time.time()
+  print "Done getting %s, took %d seconds" % (hdfs_path, end - start)
 
 def hdfs_push_file(local_path, hdfs_path):
-  print "Putting %s..."
+  print "Putting %s..." % (local_path)
+  start = time.time()
   command = "hadoop fs -put %s %s" % (local_path, hdfs_path)
   subprocess.call(shlex.split(command))
-  print "Done putting %s..."
+  end = time.time()
+  print "Done putting %s, took %d seconds" % (local_path, end - start)
